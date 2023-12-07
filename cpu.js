fa7509aed2a07129bdcb7ffa3a82df41a3487b83
Lillian Ye <lillianye@princeton.edu>
2023-12-07 18:03:54 -0500

    blehh

--- cpu.js
+++ cpu.js
@@ -1,7 +1,14 @@
 
 chrome.system.cpu.getInfo(function(info) {
-  var cpuInfo = info.modelName;
-  console.log(cpuInfo);
-  document.getElementById('numOfProcessors').innerText = 'CPU Info: ' + cpuInfo;
+  var cpuModel = info.modelName;
+  var numCPU = info.numOfProcessors;
+  var arch = info.archName;
+  var temps = info.temperatures;
+  var processor = JSON.stringify(info.processors);
+  // console.log(JSON.stringify(info));
+  // console.log(info.total);
+  // console.log(cpuInfo);
+  var message = 'CPU Model: ' + cpuModel + ' Number of CPUs: ' + numCPU + ' CPU Architecture: ' + arch;
+  document.getElementById('numOfProcessors').innerText = message;
 });
 
