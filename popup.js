d10b0f442caa1d69cedabaeadba4aa03e3b2b76f
Ankhitha Manjunatha <am5897@princeton.edu>
2023-12-07 10:05:35 -0500

    update ui with post requests

--- popup.js
+++ popup.js
@@ -1,1 +1,6 @@
-console.log('This is a popup!');
+chrome.runtime.onMessage.addListener(function(message, sender, sendResponse) {
+    if (message.numberOfPostRequests !== undefined) {
+      var numberOfPostRequests = message.numberOfPostRequests;
+      document.getElementById('numberOfPostRequestsDisplay').innerText = 'Number of Post Requests: ' + numberOfPostRequests;
+    }
+  });
--- service-worker.js
+++ service-worker.js
@@ -1,6 +1,10 @@
+var numberOfPostRequests = 0;
+
 chrome.webRequest.onCompleted.addListener(
   function(details) {
     if (details.method === "POST") {
+      numberOfPostRequests++;
+      chrome.runtime.sendMessage({ numberOfPostRequests: numberOfPostRequests });
       chrome.notifications.create({
         type: 'basic',
         iconUrl: 'next_try.png',
