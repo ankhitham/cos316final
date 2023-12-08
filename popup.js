d6122dbe129947ad044d53d2c4a4685ae75f3617
Ankhitha Manjunatha <am5897@princeton.edu>
2023-12-08 11:39:56 -0500

    popup

--- popup.js
+++ popup.js
@@ -1,16 +1,9 @@
-// chrome.runtime.onMessage.addListener(function(message, sender, sendResponse) {
-//     if (message.numberOfPostRequests !== undefined) {
-//       var numberOfPostRequests = message.numberOfPostRequests;
-//       document.getElementById('numberOfPostRequestsDisplay').innerText = 'Number of Post Requests: ' + numberOfPostRequests;
-//     }
-//     return true;
-//   });
-
 chrome.storage.local.get(['numberOfPostRequests'], function(result) {
   var numberOfPostRequests = result.numberOfPostRequests;
   
   if (numberOfPostRequests !== undefined) {
       console.log("Number of POST requests in content script: " + numberOfPostRequests);
+      document.getElementById('numberOfPostRequestsDisplay').innerText = 'Number of Post Requests: ' + numberOfPostRequests;
   } else {
       console.error("Number of POST requests is undefined in content script.");
   }
