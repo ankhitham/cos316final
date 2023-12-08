47ad2219d474f3070a6d9b226147dcd962902bf2
Ankhitha Manjunatha <am5897@princeton.edu>
2023-12-08 11:13:52 -0500

    get info from background to content

--- service-worker.js
+++ service-worker.js
@@ -1,4 +1,5 @@
 var numberOfPostRequests = 0;
+chrome.storage.local.set({ 'numberOfPostRequests': 0 });
 var startTime = 0;
 
 chrome.webRequest.onResponseStarted.addListener(
@@ -13,16 +14,8 @@ chrome.webRequest.onCompleted.addListener(
   function(details) {
     if (details.method === "POST") {
       numberOfPostRequests++;
-      // chrome.runtime.sendMessage(null, { numberOfPostRequests: numberOfPostRequests })
-      // .then(response => {
-      //   // Handle the response if needed
-      // })
-      // .catch(error => {
-      //   console.error('Error in chrome.runtime.sendMessage:', error);
-      // });
-      // chrome.tabs.query({active: true, currentWindow: true }, function(tabs) {
-      //   chrome.tabs.sendMessage(tabs[0].id, { numberOfPostRequests: numberOfPostRequests });
-      // });
+      console.log("post requests background: " + numberOfPostRequests)
+      chrome.storage.local.set({'numberOfPostRequests': numberOfPostRequests });
 
       const loadTime = details.timeStamp - startTime;
       chrome.notifications.create({
