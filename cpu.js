ea8f986f50de6e39849d67c160fbad9d7beefbe2
Tara Agarwal <taraagarwal@dynamic-oit-vapornet100-10-8-92-6.princeton.edu>
2023-12-08 10:50:21 -0500

    all memory info

--- cpu.js
+++ cpu.js
@@ -3,12 +3,12 @@ chrome.system.cpu.getInfo(function(info) {
   var cpuModel = info.modelName;
   var numCPU = info.numOfProcessors;
   var arch = info.archName;
-  var temps = info.temperatures;
+  var temps = JSON.stringify(info.temperatures);
   var processor = JSON.stringify(info.processors);
   // console.log(JSON.stringify(info));
   // console.log(info.total);
   // console.log(cpuInfo);
-  var message = 'CPU Model: ' + cpuModel + ' Number of CPUs: ' + numCPU + ' CPU Architecture: ' + arch;
+  var message = 'CPU Model: ' + cpuModel + ' Number of CPUs: ' + numCPU + ' CPU Architecture: ' + arch + 'Temp: '+ temps
   document.getElementById('numOfProcessors').innerText = message;
 });
 
